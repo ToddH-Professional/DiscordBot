addf055d7ede877fe11a788699292ded2f683d3f
PCName <PCName@DESKTOP-I8HOHL2>
2025-02-18 19:47:05 -0600

    testing

--- discordBot.py
+++ discordBot.py
@@ -2,6 +2,11 @@ import discord
 import os
 import requests
 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 intents = discord.Intents.default()
 intents.message_content = True  # Enable reading message content
 
